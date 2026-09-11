76678ea4ccf3f7b92ad2e6a2b15949d6dfe375cf
marco-siboldi <siboldi@outlook.com.ar>
2026-09-11 20:40:10 -0300

    feat(clase01/ejercicio03): implementar NodoPagina

---
+++ src/ar/edu/unlu/poo/clase01/ejercicio03_historial/NodoPagina.java
@@ -0,0 +1,19 @@
+package ar.edu.unlu.poo.clase01.ejercicio03_historial;
+
+public class NodoPagina {
+    private PaginaWeb pagina;
+    private NodoPagina paginaAnterior = null;
+
+    public  NodoPagina(PaginaWeb pagina,  NodoPagina paginaAnterior) {
+        this.paginaAnterior = paginaAnterior;
+        this.pagina = pagina;
+    }
+
+    public PaginaWeb getPagina() {
+        return pagina;
+    }
+
+    public NodoPagina getPaginaAnterior() {
+        return paginaAnterior;
+    }
+}
