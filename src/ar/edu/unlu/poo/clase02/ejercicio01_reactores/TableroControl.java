2c6598f536198fbb38f6bad1ee3e75e57b931678
marco-siboldi <siboldi@outlook.com.ar>
2026-09-12 20:19:30 -0300

    feat(clase02/ejercicio01): implementar clase TableroControl

---
+++ src/ar/edu/unlu/poo/clase02/ejercicio01_reactores/TableroControl.java
@@ -0,0 +1,25 @@
+package ar.edu.unlu.poo.clase02.ejercicio01_reactores;
+
+import java.util.ArrayList;
+
+public class TableroControl {
+
+    private ArrayList<Reactor> reactores;
+    private String nombrePlanta;
+
+    public TableroControl(String nombre) {
+        this.reactores = new ArrayList<>();
+        this.nombrePlanta = nombre;
+    }
+
+    public void agregarReactor(Reactor reactor) {
+        if(reactor == null){
+            throw new  NullPointerException("Reactor no puede ser nulo.");
+        }
+        this.reactores.add(reactor);
+    }
+
+    public String getNombrePlanta() {
+        return this.nombrePlanta;
+    }
+}
