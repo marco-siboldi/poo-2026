c4603bb544b86a242f044e69c43e046bfc50d6b9
marco-siboldi <siboldi@outlook.com.ar>
2026-09-12 20:19:42 -0300

    feat(clase02/ejercicio01): implementar TemperaturaCriticaException

---
+++ src/ar/edu/unlu/poo/clase02/ejercicio01_reactores/TemperaturaCriticaException.java
@@ -0,0 +1,7 @@
+package ar.edu.unlu.poo.clase02.ejercicio01_reactores;
+
+public class TemperaturaCriticaException extends RuntimeException {
+    public TemperaturaCriticaException(String mensaje) {
+        super(mensaje);
+    }
+}
