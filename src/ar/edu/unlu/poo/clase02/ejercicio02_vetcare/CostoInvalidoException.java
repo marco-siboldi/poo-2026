2aa9e16aa118fd9cbe39d721a0bd5cc42726c3cc
marco-siboldi <siboldi@outlook.com.ar>
2026-09-13 00:00:04 -0300

    feat(clase02/ejercicio02): implementar CostoInvalidoException y MascotaNoEncontradaException

---
+++ src/ar/edu/unlu/poo/clase02/ejercicio02_vetcare/CostoInvalidoException.java
@@ -0,0 +1,7 @@
+package ar.edu.unlu.poo.clase02.ejercicio02_vetcare;
+
+public class CostoInvalidoException extends RuntimeException {
+    public CostoInvalidoException(String s) {
+        super(s);
+    }
+}
---
+++ src/ar/edu/unlu/poo/clase02/ejercicio02_vetcare/MascotaNoEncontradaException.java
@@ -0,0 +1,7 @@
+package ar.edu.unlu.poo.clase02.ejercicio02_vetcare;
+
+public class MascotaNoEncontradaException extends RuntimeException {
+    public MascotaNoEncontradaException(String s) {
+        super(s);
+    }
+}
