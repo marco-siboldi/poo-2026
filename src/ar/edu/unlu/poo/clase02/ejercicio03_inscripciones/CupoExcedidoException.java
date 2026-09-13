217f1f7a8be687d63041524fc3a0f57bcba7897c
marco-siboldi <siboldi@outlook.com.ar>
2026-09-13 13:30:04 -0300

    feat(clase02/ejercicio03): implementar CupoExcedidoException y EstudianteYaInscriptoException

---
+++ src/ar/edu/unlu/poo/clase02/ejercicio03_inscripciones/CupoExcedidoException.java
@@ -0,0 +1,7 @@
+package ar.edu.unlu.poo.clase02.ejercicio03_inscripciones;
+
+public class CupoExcedidoException extends RuntimeException {
+    public CupoExcedidoException(String s) {
+        super(s);
+    }
+}
---
+++ src/ar/edu/unlu/poo/clase02/ejercicio03_inscripciones/EstudianteYaInscriptoException.java
@@ -0,0 +1,7 @@
+package ar.edu.unlu.poo.clase02.ejercicio03_inscripciones;
+
+public class EstudianteYaInscriptoException extends RuntimeException {
+    public EstudianteYaInscriptoException(String s) {
+        super(s);
+    }
+}
